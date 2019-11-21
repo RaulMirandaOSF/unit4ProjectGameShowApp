d8ff52361aa424ced8f20d062c594e54c6eb83e4
RaulMirandaOSF <raul.miranda@osf-commerce.com>
2019-11-21 12:12:53 -0300

    Done addPhraseToDisplay method.

--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -1,3 +1,27 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+        this.letterSelected = '';
+    }
+
+    addPhraseToDisplay() {
+        let phraseHtml = this.phrase.split('').map(c => {
+            let className = c === " " ? "space" : `hide letter ${c}`; 
+            return `<li class="${className}">${c}</li>`
+        }).join('');
+
+        $('#phrase ul').html(phraseHtml);
+    }
+
+    checkLetter() {
+        return this.phrase.includes(this.letterSelected);
+    }
+
+    showMatchedLetter() {
+
+    }
+}
--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -1,3 +1,6 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+ let test = new Phrase('Lov U');
+ test.addPhraseToDisplay();
