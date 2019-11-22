51f631598a454b6bf292ff0c1e3058d9067f0284
RaulMirandaOSF <raul.miranda@osf-commerce.com>
2019-11-22 11:23:23 -0300

    Done until "Resetting the gameboard between games".

--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -3,9 +3,28 @@
  * app.js */
 
 let game = new Game();
-game.startGame();
+
+$("#btn__reset").click(function () {    
+    resetKeyrow();
+    resetOverlay();
+    $("#scoreboard ol li img").attr("src", "images/liveHeart.png");
+    
+    game = new Game();
+    game.startGame();
+});
 
 $(".keyrow button").click(function (e) {
     game.handleInteraction(e.target);
 });
 
+function resetKeyrow() {
+    $(".keyrow button").removeClass("chosen");
+    $(".keyrow button").removeClass("wrong");
+    $(".keyrow button").addClass("key");
+}
+
+function resetOverlay() {
+    $("#overlay").addClass("start");
+    $("#overlay").removeClass("win");
+    $("#overlay").removeClass("lose");
+}
